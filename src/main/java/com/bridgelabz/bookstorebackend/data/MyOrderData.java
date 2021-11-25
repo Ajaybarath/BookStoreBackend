e0deb536b5b35ec4b159755c2c9bba3c112cc192
Swathi <swathi.h@perfios.com>
2021-11-25 20:21:46 +0530

    MODIFIED: Additional fields added along with constructor to model

--- src/main/java/com/bridgelabz/bookstorebackend/data/MyOrderData.java
+++ src/main/java/com/bridgelabz/bookstorebackend/data/MyOrderData.java
@@ -2,16 +2,26 @@ package com.bridgelabz.bookstorebackend.data;
 
 import java.time.LocalDate;
 
+//import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
+import com.bridgelabz.bookstorebackend.dto.MyOrderDTO;
+
+import lombok.Data;
+
 @Entity
 @Table(name = "my_order")
-public class MyOrderData {
+public @Data class MyOrderData {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "order_id")
 	int orderId;
 
 	@JoinColumn(name = "customer.customer_id")
@@ -19,8 +29,22 @@ public class MyOrderData {
 
 	@JoinColumn(name = "books.book_id")
 	int bookId;
-	
+
+	int totalCost;
+
 	int quantity;
 
 	LocalDate orderDate;
+
+	public MyOrderData() {
+
+	}
+
+	public MyOrderData(int customerId, MyOrderDTO myOrderDTO) {
+		this.customerId = customerId;
+		this.bookId = myOrderDTO.getBookId();
+		this.totalCost = myOrderDTO.getTotalCost();
+		this.quantity = myOrderDTO.getQuantity();
+		this.orderDate = myOrderDTO.getOrderDate();
+	}
 }
--- src/main/java/com/bridgelabz/bookstorebackend/dto/MyOrderDTO.java
+++ src/main/java/com/bridgelabz/bookstorebackend/dto/MyOrderDTO.java
@@ -1,8 +1,17 @@
 package com.bridgelabz.bookstorebackend.dto;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 public @Data class MyOrderDTO {
-
 	
+	int bookId;
+	int totalCost;
+	int quantity;
+	
+	@JsonFormat(pattern="dd MMM yyyy")
+	LocalDate orderDate;
 }
