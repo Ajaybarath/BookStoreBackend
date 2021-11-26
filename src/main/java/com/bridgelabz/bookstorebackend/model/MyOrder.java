8e6bfc3e25696d2a90d32d8a3320ed19880a34be
Swathi <swathi.h@perfios.com>
2021-11-26 14:37:08 +0530

    DELETED: Unwanted import

--- src/main/java/com/bridgelabz/bookstorebackend/model/MyOrder.java
+++ src/main/java/com/bridgelabz/bookstorebackend/model/MyOrder.java
@@ -2,7 +2,6 @@ package com.bridgelabz.bookstorebackend.model;
 
 import java.time.LocalDate;
 
-//import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
