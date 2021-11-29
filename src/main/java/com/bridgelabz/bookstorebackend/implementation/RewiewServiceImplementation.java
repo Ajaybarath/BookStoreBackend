8f5af678bee2ea1dcdf1eb18dfe6facf5fe59fc5
ajaybarath <ajaybarath3@gmail.com>
2021-11-29 21:24:42 +0530

    Refractored: customer to user

--- src/main/java/com/bridgelabz/bookstorebackend/implementation/RewiewServiceImplementation.java
+++ src/main/java/com/bridgelabz/bookstorebackend/implementation/RewiewServiceImplementation.java
@@ -3,6 +3,7 @@ package com.bridgelabz.bookstorebackend.implementation;
 import com.bridgelabz.bookstorebackend.model.Review;
 import com.bridgelabz.bookstorebackend.dto.ReviewDTO;
 import com.bridgelabz.bookstorebackend.repository.ReviewRepository;
+import com.bridgelabz.bookstorebackend.service.BooksService;
 import com.bridgelabz.bookstorebackend.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ public class RewiewServiceImplementation implements ReviewService {
     @Autowired
     ReviewRepository reviewRepository;
 
+    @Autowired
+    BooksService booksService;
+
     @Override
     public List<Review> getAllRewiewsByBook(int bookId) {
         return reviewRepository.findByBookId(bookId);
@@ -23,6 +27,13 @@ public class RewiewServiceImplementation implements ReviewService {
     @Override
     public Review createReview(ReviewDTO reviewDTO) {
         Review review = new Review(reviewDTO);
+        List<Review> ratingList = reviewRepository.findByBookId(reviewDTO.getBookId());
+        int sum = ratingList.stream().mapToInt(Review::getRating).sum();
+        sum += reviewDTO.getRating();
+        int noOfRating = reviewRepository.findByBookId(reviewDTO.getBookId()).size();
+        noOfRating++;
+        double rating = sum/(double)noOfRating;
+        booksService.updateRating(reviewDTO.getBookId(), rating, noOfRating);
         return reviewRepository.save(review);
     }
 }
--- src/main/java/com/bridgelabz/bookstorebackend/service/CustomerServiceIF.java
+++ src/main/java/com/bridgelabz/bookstorebackend/service/CustomerServiceIF.java
@@ -1,23 +1,23 @@
 package com.bridgelabz.bookstorebackend.service;
 
 import com.bridgelabz.bookstorebackend.model.Address;
-import com.bridgelabz.bookstorebackend.model.Customer;
+import com.bridgelabz.bookstorebackend.model.User;
 import com.bridgelabz.bookstorebackend.dto.AddressDTO;
 import com.bridgelabz.bookstorebackend.dto.LoginDTO;
 import com.bridgelabz.bookstorebackend.dto.SignUpDTO;
 
 public interface CustomerServiceIF {
 
-	Customer signUpCustomer(SignUpDTO signUpDTO);
+	User signUpCustomer(SignUpDTO signUpDTO);
 
-	Customer loginCustomer(LoginDTO loginDTO);
+	User loginCustomer(LoginDTO loginDTO);
 
-	Customer getCustomerById(int customerId);
+	User getCustomerById(int customerId);
 
 	Address addAddress(int customerId, AddressDTO addressDTO);
 
 	Address updateAddress(int customerId, AddressDTO addressDTO);
 
-	Customer getPassword(String email);
+	User getPassword(String email);
 
 }
