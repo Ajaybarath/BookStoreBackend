f56de1f321c98bcb95de330739901cd9c075d72f
ajaybarath <ajaybarath3@gmail.com>
2021-11-29 21:22:45 +0530

    Added: crossorigin policy

--- src/main/java/com/bridgelabz/bookstorebackend/controller/CartController.java
+++ src/main/java/com/bridgelabz/bookstorebackend/controller/CartController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/bookstore/cart")
 public class CartController {
