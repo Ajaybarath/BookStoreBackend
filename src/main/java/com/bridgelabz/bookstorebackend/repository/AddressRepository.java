fc3ea23f49a1309db40964be8fecd2e038a08fd6
Swathi <swathi.h@perfios.com>
2021-11-30 21:52:25 +0530

    DELETED: Codes that caused merge conflicts

--- src/main/java/com/bridgelabz/bookstorebackend/repository/AddressRepository.java
+++ src/main/java/com/bridgelabz/bookstorebackend/repository/AddressRepository.java
@@ -9,11 +9,6 @@ import com.bridgelabz.bookstorebackend.model.Address;
 
 public interface AddressRepository extends JpaRepository<Address, UUID>{
 
-<<<<<<< HEAD
 	@Query(value = "SELECT * FROM address WHERE user_id = :userId AND address_type = :addressType", nativeQuery = true)
 	Address getAddress(int userId, String addressType);
-=======
-	@Query(value = "SELECT * FROM address WHERE customer_id = :customerId AND address_type = :addressType", nativeQuery = true)
-	Address getAddress(UUID customerId, String addressType);
->>>>>>> localbranch
 }
