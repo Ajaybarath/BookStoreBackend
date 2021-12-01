eb7f19eb5c01884518731154d0d176414f2108da
Swathi <swathi.h@perfios.com>
2021-12-01 18:15:23 +0530

    REFACTORED: Removing unwanted spaces and imports

--- src/main/java/com/bridgelabz/bookstorebackend/model/Cart.java
+++ src/main/java/com/bridgelabz/bookstorebackend/model/Cart.java
@@ -12,79 +12,77 @@ import java.util.List;
 @Table(name = "cart")
 @Getter
 @Setter
-public
-@ToString
-class Cart {
+public @ToString class Cart {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "cart_id")
-    int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "cart_id")
+	int id;
 
-    int userId;
+	int userId;
 
-    int quantity;
+	int quantity;
 
-    int totalPrice;
+	int totalPrice;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    List<CartItem> cartItems = new ArrayList<>();
+	@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+	List<CartItem> cartItems = new ArrayList<>();
 
-    public Cart(int userID, int quantity, int totalPrice) {
-        this.userId = userID;
-        this.quantity = quantity;
-        this.totalPrice = totalPrice;
-    }
+	public Cart(int userID, int quantity, int totalPrice) {
+		this.userId = userID;
+		this.quantity = quantity;
+		this.totalPrice = totalPrice;
+	}
 
-    public void addCartItem(CartItem cartItem) {
-        cartItems.add(cartItem);
-    }
+	public void addCartItem(CartItem cartItem) {
+		cartItems.add(cartItem);
+	}
 
-    public void removeCartItem(CartItem cartItem) {
-        cartItems.remove(cartItem);
-    }
+	public void removeCartItem(CartItem cartItem) {
+		cartItems.remove(cartItem);
+	}
 
-    public void addQuantity(Books books, int quantity) {
-        if (cartItems.stream().anyMatch(it -> it.getBookId() == books.getId())) {
-            cartItems.forEach((cartItem -> {
-                if (cartItem.getBookId() == books.getId()) {
-                    int qty = cartItem.getQuantity();
-                    qty += quantity;
-                    cartItem.setQuantity(qty);
-                    cartItem.setTotal(qty * cartItem.getPrice());
-                    this.quantity += quantity;
-                    this.totalPrice += quantity * cartItem.getPrice();
-                }
-            }));
-        } else {
-            CartItem cartItem = new CartItem(quantity, books.getId(), books.getPrice(), books.getPrice());
-            this.quantity += quantity;
-            this.totalPrice += quantity * cartItem.getPrice();
-            this.addCartItem(cartItem);
-            cartItem.setCart(this);
-        }
-    }
+	public void addQuantity(Books books, int quantity) {
+		if (cartItems.stream().anyMatch(it -> it.getBookId() == books.getId())) {
+			cartItems.forEach((cartItem -> {
+				if (cartItem.getBookId() == books.getId()) {
+					int qty = cartItem.getQuantity();
+					qty += quantity;
+					cartItem.setQuantity(qty);
+					cartItem.setTotal(qty * cartItem.getPrice());
+					this.quantity += quantity;
+					this.totalPrice += quantity * cartItem.getPrice();
+				}
+			}));
+		} else {
+			CartItem cartItem = new CartItem(quantity, books.getId(), books.getPrice(), books.getPrice());
+			this.quantity += quantity;
+			this.totalPrice += quantity * cartItem.getPrice();
+			this.addCartItem(cartItem);
+			cartItem.setCart(this);
+		}
+	}
 
-    public void decreaseQuantity(Books books, int quantity) {
-        if (cartItems.stream().anyMatch(it -> it.getBookId() == books.getId())) {
-            cartItems.forEach((cartItem -> {
-                if (cartItem.getBookId() == books.getId()) {
-                    int qty = cartItem.getQuantity();
-                    qty -= quantity;
-                    cartItem.setQuantity(qty);
-                    cartItem.setTotal(qty * cartItem.getPrice());
+	public void decreaseQuantity(Books books, int quantity) {
+		if (cartItems.stream().anyMatch(it -> it.getBookId() == books.getId())) {
+			cartItems.forEach((cartItem -> {
+				if (cartItem.getBookId() == books.getId()) {
+					int qty = cartItem.getQuantity();
+					qty -= quantity;
+					cartItem.setQuantity(qty);
+					cartItem.setTotal(qty * cartItem.getPrice());
 
-                    this.quantity -= quantity;
-                    this.totalPrice -= quantity * cartItem.getPrice();
-                }
-            }));
-        }
+					this.quantity -= quantity;
+					this.totalPrice -= quantity * cartItem.getPrice();
+				}
+			}));
+		}
 
-        cartItems.removeIf(ob -> ob.getQuantity() <= 0);
+		cartItems.removeIf(ob -> ob.getQuantity() <= 0);
 
-    }
+	}
 
-    public Cart() {
-    }
+	public Cart() {
+	}
 
 }
