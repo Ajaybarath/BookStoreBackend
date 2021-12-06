0c37e0f150faf48fc35e86043aac390801260f94
ajaybarath <ajaybarath3@gmail.com>
2021-12-06 19:55:27 +0530

    Added: additional field to get the data by email

--- src/main/java/com/bridgelabz/bookstorebackend/repository/CustomerRepository.java
+++ src/main/java/com/bridgelabz/bookstorebackend/repository/CustomerRepository.java
@@ -1,14 +1,15 @@
 package com.bridgelabz.bookstorebackend.repository;
 
+import com.bridgelabz.bookstorebackend.model.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.bridgelabz.bookstorebackend.model.User;
-
-public interface CustomerRepository extends JpaRepository<User, Integer>{
+public interface CustomerRepository extends JpaRepository<ApplicationUser, Integer>{
 	@Query(value = "SELECT * FROM user WHERE email = :email AND password = :password", nativeQuery = true)
-    User loginCustomer(String email, String password);
+    ApplicationUser loginCustomer(String email, String password);
 
 	@Query(value = "SELECT * FROM user WHERE email = :email", nativeQuery = true)
-    User getPassword(String email);
+    ApplicationUser getPassword(String email);
+
+	ApplicationUser findUserByEmail(String email);
 }
